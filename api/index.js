b4889bb802c3505b2dbc6925daec2c1b8caabb58
Arpit Sharma <arpit.s@neodove.com>
2023-01-23 11:21:40 +0530

    post request added

--- api/index.js
+++ api/index.js
@@ -1,43 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { v4: uuidv4 } = require('uuid');
-var cors = require('cors');
+const { v4: uuidv4 } = require("uuid");
+var cors = require("cors");
 
 let currentUUID = "";
 let oldUUID = "";
 let appRunningFirstTime = true;
 
-app.use(cors())
+app.use(cors());
 
-app.get('/api', function (req, res) {
+app.get("/api", function (req, res) {
     uuidString();
-  res.send('Hello World');
-})
+    res.send("Hello World");
+});
 
-app.get('/api/getqrcode', (req, res) => {
-  uuidString();
-  console.log("Inbound request received");
+app.get("/api/getqrcode", (req, res) => {
+    uuidString();
+    console.log("Inbound request received");
     return res.status(200).json({
-        response: `http://localhost:8080/?link=${currentUUID}`
+        response: `https://attendify-frontend-1o31.vercel.app/markattendance?id="${currentUUID}"`,
     });
-})
+});
+
+app.post("/api/checkqrcode", (req, res) => {
+    console.log("Check qr code request received");
+    let error;
+    let status;
+    try {
+        const { queryId } = req.body;
+        console.log("id");
+        console.log(queryId);
+        if (queryId["id"] == currentUUID) {
+            status = "success";
+        }
+    } catch (e) {
+        console.error("Error:", error);
+        status = "failure";
+    }
+
+    console.log("Inbound request received");
+
+    res.json({ error, status });
+
+    // return res.status(200).json({
+    //     response: `http://localhost:3000/?id=${currentUUID}`,
+    // });
+});
 
 const uuidString = () => {
     console.log("uuid function was called");
-    if(currentUUID != "") {
+    if (currentUUID != "") {
         oldUUID = currentUUID;
     }
     currentUUID = uuidv4();
     console.log(currentUUID);
-}
+};
 
 //PORT
 const port = 4000;
 
 //new
 
-module.exports = app;
+// //Starting a server
+// app.listen(port, () => {
+//     console.log(`app is running at ${port}`);
+//     // if(appRunningFirstTime == true) {
+//     //   uuidString();
+//     //   appRunningFirstTime = false;
+//     // }
+//     // setInterval(() => uuidString(), 8000)
+// });
 
+module.exports = app;
 
 // const app = require('express')();
 // const { v4 } = require('uuid');
